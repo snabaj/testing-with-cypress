6da86edd6c04ed4ea56d907961432ccb1a7c1dd4
Star <snabajja@gmail.com>
2025-03-15 13:14:41 -0400

    Update cypress.config.ts to include baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
       bundler: "vite",
     },
     specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
-  }
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:3001",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
